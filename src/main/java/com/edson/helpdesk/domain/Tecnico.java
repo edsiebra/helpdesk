435780f46d80a2dfefc562246e9b29181be860f5
Edson Siebra <edsiebra@gmail.com>
2023-12-02 08:19:19 -0300

    endpoint atualizado

--- src/main/java/com/edson/helpdesk/domain/Tecnico.java
+++ src/main/java/com/edson/helpdesk/domain/Tecnico.java
@@ -2,7 +2,9 @@ package com.edson.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import com.edson.helpdesk.domain.dtos.TecnicoDTO;
 import com.edson.helpdesk.domain.enums.Perfil;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -27,6 +29,17 @@ public class Tecnico extends Pessoa{
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
 		addPerfil(Perfil.CLIENTE);
+	}
+	
+	public Tecnico(TecnicoDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 	public List<Chamado> getChamados() {
